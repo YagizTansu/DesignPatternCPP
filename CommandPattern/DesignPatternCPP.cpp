2140f9d56d93a4553c48c27f31aac84b5b71be51
yagiztansu <yagiz.tansu99@gmail.com>
2022-08-14 12:00:44 +0300

    Command pattern added

---
+++ CommandPattern/DesignPatternCPP.cpp
@@ -0,0 +1,12 @@
+﻿// DesignPatternCPP.cpp : Defines the entry point for the application.
+//
+
+#include "DesignPatternCPP.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
