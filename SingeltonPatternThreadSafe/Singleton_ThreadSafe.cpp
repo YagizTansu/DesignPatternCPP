237daa12e0ebb81856b435e861ded20de27df45b
yagiztansu <yagiz.tansu99@gmail.com>
2022-08-28 11:30:20 +0300

    Add singleton thread safe

---
+++ SingeltonPatternThreadSafe/Singleton_ThreadSafe.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+
+class Singleton {
+private:
+	static Singleton* singleInstance; //// The private reference to the one and only instance.
+	static mutex lock;
+	Singleton() {}// No client can instantiate a Singleton object!
+public:
+
+	// Creates the instance if it does not yet exist.
+	// Returns a reference to the single instance.
+	static Singleton* getInstance() {
+
+
+
+		if (singleInstance == NULL) {
+			lock.lock();
+			if (singleInstance == NULL)
+				singleInstance = new Singleton();
+
+			lock.unlock();
+		}
+
+		return singleInstance;
+	}
+};
+
+Singleton* Singleton::singleInstance = NULL;
+mutex Singleton::lock;
+
+
+int main() {
+
+	Singleton* instance1;
+	Singleton* instance2;
+	Singleton* instance3;
+
+	instance1 = Singleton::getInstance();
+	instance2 = Singleton::getInstance();
+	instance3 = Singleton::getInstance();
+
+	if (instance1 == instance2 && instance1 == instance3)
+		cout << "Singleton Pattern Worked" << endl;
+
+	return 0;
+}
